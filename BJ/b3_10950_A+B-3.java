90abdf1ebc5eda53d12ce76e33be20617076ae13
hxxejx <uiu1442@naver.com>
2022-05-10 23:53:08 +0900

    Create b3_10950_A+B-3.java

---
+++ BJ/b3_10950_A+B-3.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			sb.append(A+B).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
