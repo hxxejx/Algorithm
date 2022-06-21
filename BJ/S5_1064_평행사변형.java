64ba297d960adde66ae209f4d021c606de6a7d34
hxxejx <uiu1442@naver.com>
2022-06-21 21:18:39 +0900

    0621

---
+++ BJ/S5_1064_평행사변형.java
@@ -0,0 +1,15 @@
+package 문제풀이;
+
+import java.io.*;
+import java.util.*;
+
+public class S5_1064_평행사변형 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+
+	}
+
+}
