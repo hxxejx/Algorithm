4cfdc9e2e7e67b93b352994f7fe9cafab4789f4a
hxxejx <uiu1442@naver.com>
2022-07-31 21:40:05 +0900

    0731

---
+++ BJ/S1_12761_돌다리.java
@@ -0,0 +1,15 @@
+package 문제풀이;
+
+import java.io.*;
+import java.util.*;
+
+public class S1_12761_돌다리 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+
+	}
+
+}
