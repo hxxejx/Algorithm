72e62c0eec0c5a67d1760e47c3a7637fe2a7c2cf
허은지 <uiu1442@naver.com>
2022-11-30 23:28:33 +0900

    1130

---
+++ STUDY/1130/G3_14890_경사로.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class G3_14890_경사로 {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		
+	}
+
+}
